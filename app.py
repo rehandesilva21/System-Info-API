22f9694d8626c82f84e9e2324223c4ef4d2c5bd9
Dreamscape <rehandesilva@Rehans-MacBook-Air.local>
2024-08-20 14:44:02 +0530

    add-folders

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, render_template
+import psutil
+import platform
+import socket
+
+app = Flask(__name__,template_folder='system')
+
+@app.route('/')
+def index():
+    return render_template('system.html')
+
+@app.route('/api/system_info', methods=['GET','POST'])
+def system_info():
+    cpu_usage = psutil.cpu_percent(interval=1)
+    ram_usage = psutil.virtual_memory().percent
+    net_io = psutil.net_io_counters()
+    network_usage = {
+        'bytes_sent': net_io.bytes_sent,
+        'bytes_recv': net_io.bytes_recv
+    }
+    
+    system_info = {
+        'processor_name': platform.processor(),
+        'os_version': platform.platform(),
+        'cpu_usage': cpu_usage,
+        'ram_usage': ram_usage,
+        'network_usage': network_usage
+    }
+    
+    return jsonify(system_info)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5100)
